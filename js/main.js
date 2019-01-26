8b5e6e1ed2a05f02f6bcd9de9bf8671dbbcf6bd5
Jakša Mališić <jaksa.malisic@gmail.com>
2019-01-27 00:28:30 +0100

    scroll to top added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 var owl = $(".owl-carousel");
+const scrollToTop = document.querySelector("#scrollToTop");
+scrollToTop.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
+
+document.addEventListener("scroll", () => {
+  if (window.pageYOffset >= 1900) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+});
+
 owl.owlCarousel({
   items: 1,
   loop: true,
