3d652232a6950820b5bc0125f0ce07f01f2c0102
Jaksa Malisic <jaksa.malisic@gmail.com>
2019-01-27 11:16:46 +0100

    dynamic link rendering added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,56 @@
 var owl = $(".owl-carousel");
 const scrollToTop = document.querySelector("#scrollToTop");
 const nav = document.querySelector("nav");
+const navigacija = document.querySelector("#navigacija");
+const botNav = document.querySelector("#botNav");
+const botNavMisc = document.querySelector("#botNavMisc");
 
 scrollToTop.addEventListener("click", () => {
   window.scrollTo(0, 0);
 });
+
+// Dinamicko ispisivanje linkova
+const topNavLink = (href, text) => `
+<li>
+  <a
+    href="${href}"
+    class="h-full px-4 no-underline text-orange-darker uppercase tracking-wide font-bold text-sm"
+  >
+  ${text}
+  </a>
+</li>
+`;
+
+const botNavLink = (href, text) => `
+<li class="my-8">
+  <a
+    href="${href}"
+    class="text-base text-white font-bold uppercase no-underline"
+    >${text}</a
+  >
+</li>
+`;
+
+fetch("/data/links.json")
+  .then(res => res.json())
+  .then(links => {
+    const { navigation, misc } = links;
+    const navigacijaMarkup = navigation
+      .map(({ href, text }) => topNavLink(href, text))
+      .join(" ");
+
+    const botNavMarkup = navigation
+      .map(({ href, text }) => botNavLink(href, text))
+      .join(" ");
+
+    const miscMarkup = misc
+      .map(({ href, text }) => botNavLink(href, text))
+      .join(" ");
+
+    navigacija.innerHTML = navigacijaMarkup;
+    botNav.innerHTML = botNavMarkup;
+    botNavMisc.innerHTML = miscMarkup;
+  });
 
 document.addEventListener("scroll", () => {
   if (window.pageYOffset >= 150) {
